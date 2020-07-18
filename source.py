6b0be5c524d2e115be6b52c5f992a16187e12eea
Shubham Dubey <sdubey504@gmail.com>
2020-07-19 02:16:30 +0530

    added formatter

--- source.py
+++ source.py
@@ -6,6 +6,7 @@ from os import remove
 
 #
 # will remove the blacklisted syscalls
+# TODO: add comment support in blacklist file
 #
 def filter_blacklist(data):
     # removing the first syscall since its always execve of the process
@@ -14,40 +15,52 @@ def filter_blacklist(data):
     data.pop(len(data)-1)
     with open('blacklist.txt') as f:
         blacklist = f.read().splitlines()
-    print(len(data))
     i = 0
     while i < len(data):
         if data[i]['syscall'] in blacklist:
             data.pop(i)
             i = 0
         i = i + 1
-
     return data
 
 
-def formatter(syscall):
-    x = {
-    "syscall": "John",
-    "age": 30,
-    "married": True,
-    "divorced": False,
-    "children": ("Ann","Billy"),
-    "pets": None,
-    "cars": [
-    {"model": "BMW 230", "mpg": 27.5},
-    {"model": "Ford Edge", "mpg": 24.1}
-    ]
+def formatter(entry):
+    no_of_args = len(entry['args'])
+    print(no_of_args)
+    i = '10'
+    entry_list = {
+    "syscall": entry['syscall'],
+    "check_args": True,
+    "next_call_is_preceding": True,
     }
-    print(syscall)
+
+    #Parsing the arguments
+    for i in range(0, no_of_args):
+        # To combine the flag dict in a single string
+        if (type(entry['args'][i]) is dict):
+            flag_arg = []
+            for j in range(0, len(entry['args'][i]['value'])):
+                if j == 0:
+                    flag_0th_string = entry['args'][i]['name']+entry['args'][i]['value'][0]
+                    flag_arg.append(flag_0th_string)
+                else:
+                    flag_arg.append(entry['args'][i]['value'][j])
+            entry_list['argument'+str(i)] = flag_arg
+
+        elif (type(entry['args'][i]) is int):
+            entry_list['argument'+str(i)] = entry['args'][i]
+        else:
+            entry_list['argument'+str(i)] = entry['args'][i]
+    #y = json.loads(x)
+    print(entry_list)
 
 def parse_strace():
     data = [json.loads(line) for line in open('input.json', 'r')]
     remove("input.json")
     data = filter_blacklist(data)
-    print(data)
-    return
-    formatter(data[0])
-    print(data[0]['syscall'])
+    for i in range(0, len(data)):
+        formatter(data[i])
+    #print(data[0]['args'][1])
 
 # use -f option in strace
 
